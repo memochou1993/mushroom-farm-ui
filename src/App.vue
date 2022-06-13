5d6d1c42bdb505e5178d31442a5c53a9f9492dad
Memo Chou <memochou1993@gmail.com>
2022-06-13 15:27:21 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container
+        fluid
+        fill-height
+        class="py-0"
+      >
+        <AppForm/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppForm from './components/AppForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppForm,
+  },
+};
+</script>
